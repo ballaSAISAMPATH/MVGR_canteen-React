4c2994b93e416e270e69826eab31adef37ec5156
SAM <saisampath453@gmail.com>
2025-07-12 16:06:33 +0530

    loading added 12/07/2025 16:06

--- client/src/pages/Admin/AdminMenuManagement/AdminMenuManagement.jsx
+++ client/src/pages/Admin/AdminMenuManagement/AdminMenuManagement.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import { setMenuItems } from '../../../store/UserInfo/store';
 import {useDispatch, useSelector} from 'react-redux'
 import MenuCardsHolder from '../../../components/MenuCardsHolder/MenuCardsHolder';
 export default function AdminMenuManagement() {
   const URL = useSelector((state)=>state.store.serverURL);
+  const [dishUploadloading,setDishUploadLoading] = useState(true);
   const dispatch = useDispatch();
-  
   useEffect(()=>{
       axios.get(URL+"/getMenuItems").then((responseList)=>{ 
       console.log(responseList.data);
@@ -15,7 +15,7 @@ export default function AdminMenuManagement() {
     
   async function adminMenuSubmit(e){
     e.preventDefault();
-    
+    setDishUploadLoading(true);
     let dishExists = await axios.post(URL+"/adminDishExists",{DishName:e.target[0].value});
     console.log("dishExists : ",dishExists.data.dishExists);
     if(!dishExists.data.dishExists){
@@ -35,7 +35,9 @@ export default function AdminMenuManagement() {
         DishImageURL:response.data.url,
       }
       let dbResponse = await axios.post(URL+"/adminMenuDishUpload",dish)
-      console.log("dish updated :",dbResponse.data.dishCreated);
+      setDishUploadLoading(false);
+      const menuList = await axios.get(URL+"/getMenuItems");
+      dispatch(setMenuItems(menuList.data));
       document.getElementById("AdminMenuManagementDishExistsText").innerHTML="<div class='text-success fs-4' ><i>dish added</i></div>";
     }
     else{
@@ -47,16 +49,16 @@ export default function AdminMenuManagement() {
 
   }
   return (
-    <div className='d-flex flex-row adminMenuManagementPageContainer   flex-wrap '>
-      <div className='adminMenuManagementUploadSection ms-2 col-12 col-md-5 col-lg-3'>
+    <div className='d-flex flex-row flex-wrap col-12 adminMenuManagementPageContainer  '>
+      <div className='adminMenuManagementUploadSection border-end border-1 border-dark-subtle col-12 col-lg-3'>
         <h4>Register a dish:</h4>
-        <form onSubmit={(event)=>{adminMenuSubmit(event)}} className='col-12 p-3' action="">
+        <form onSubmit={(event)=>{adminMenuSubmit(event)}} className='col-12 p-3 d-flex flex-column gap-3' action="">
           <div className='form-floating'>
             <input onChange={()=>{adminMenuManagementInputOnChange()}} type="text" className="form-control" id='DishName' placeholder="Dish Name"/>
             <label htmlFor="DishName">dish name</label>
           </div>
           <select className="form-select" aria-label="Default select example">
-            <option selected>select dish type</option>
+            <option value="" defaultValue>select dish type</option>
             <option value="Appetizers">Appetizers</option>
             <option value="Main Course">Main Course</option>
             <option value="Side Dishes">Side Dishes</option>
@@ -77,10 +79,20 @@ export default function AdminMenuManagement() {
           </div> 
 
           <div id="AdminMenuManagementDishExistsText"></div>
+
+          {dishUploadloading?
+          <div className='d-flex flex-row  p-1 gap-3'>
+
+          <div class="spinner-border text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
+          </div>
+          <span>dish uploading, please wait.</span>
+          </div>
+            :""}
           <input type="submit" className="AdminMenuManagementDishUploadButton btn btn-primary"/>   
         </form>
       </div>
-          <div className='adminMenuControlSection d-flex flex-row flex-wrap col-12 col-md-6 col-lg-8 '>
+          <div className='adminMenuControlSection d-flex justify-content-between flex-wrap col-12 col-lg-9  '>
             <MenuCardsHolder />  
           </div>
       </div>
